fe8dd1113243dcb08bb1e1325d6a919ad6410b7a
gogokgc <gogokgc@gmail.com>
2023-12-05 20:38:54 +0900

    Initial Commit

---
+++ BasicStudy/BasicStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicStudyApp.swift
+//  BasicStudy
+//
+//  Created by KYUCHEOL KIM on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicStudy
+//
+//  Created by KYUCHEOL KIM on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
